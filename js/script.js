e0c2681d2854131fa686ffc65ddc0f129b7443ff
nazimnk9 <nazimahmedprovat@gmail.com>
2023-11-14 00:04:12 +0600

    update toggle button color

--- js/script.js
+++ js/script.js
@@ -6,12 +6,14 @@ const image1 = document.getElementById("image1");
 const image2 = document.getElementById("image2");
 const image3 = document.getElementById("image3");
 const textBox = document.getElementById("text-box");
+const slider = document.getElementsByClassName("slider");
 
 //dark mode styles
 function darkMode(){
     nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     toggleIcon.children[0].textContent = "Dark Mode";
+    toggleIcon.children[0].style.color = "rgb(65, 255, 195)";
     toggleIcon.children[1].classList.remove("fa-sun");
     toggleIcon.children[1].classList.add("fa-moon");
     image1.src = "img/undraw_proud_coder_dark.svg";
@@ -24,6 +26,7 @@ function lightMode(){
     nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
     textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
     toggleIcon.children[0].textContent = "Light Mode";
+    toggleIcon.children[0].style.color = "rgb(202, 59, 31)";
     toggleIcon.children[1].classList.remove("fa-moon");
     toggleIcon.children[1].classList.add("fa-sun");
     image1.src = "img/undraw_proud_coder_light.svg";
